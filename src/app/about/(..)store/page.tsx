22a47124271f5732ed89423566f13b5d3f87891a
kr-aayushigupta <aayushi.g@knackroot.com>
2025-05-13 14:51:52 +0530

    commit

---
+++ src/app/about/(..)store/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Store() {
+  return (
+    <div>
+        <h2 className='text-pink-600 text-2xl text-center'>Intercepted Store from About</h2>
+    </div>
+  )
+}
+
+export default Store
